95852d8691f391dedf07020254be1e97d00ea5a0
Jan Erik Hanssen <jhanssen@gmail.com>
2021-08-08 00:31:18 -0700

    nicer

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
     declarations: [
@@ -31,7 +32,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
         MatButtonModule,
         MatFormFieldModule,
         MatInputModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        MatToolbarModule
     ],
     providers: [],
     bootstrap: [AppComponent]
