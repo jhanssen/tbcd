68037a2a941864353af33b09dad12b0225e35d16
Jan Erik Hanssen <jhanssen@gmail.com>
2021-08-19 14:03:44 -0700

    log

--- dos/main.cpp
+++ dos/main.cpp
@@ -6,12 +6,13 @@ int main(int argc, char** argv)
 {
     long int bps = 0;
     if (argc > 1) {
+        Log::log("argc is %d '%s'\n", argc, argv[1]);
         bps = atol(argv[1]);
     }
     if (bps <= 0) {
         bps = 115200L;
     }
-    Log::log("before engine\n");
+    Log::log("before engine %ld\n", bps);
     Engine* engine = new Engine(bps);
     Log::log("after engine\n");
     for (;;) {
